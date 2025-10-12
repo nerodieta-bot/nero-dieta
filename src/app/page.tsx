215939b36e18d9de978fcdc241541c791c2b29cc
Nero <ask.nero.now@gmail.com>
2025-10-12 22:11:12 +0000

    Try fixing this error: `Runtime Error: Hydration failed because the serv

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,14 @@ import { IngredientGrid } from '@/components/ingredient-grid';
 import { ingredients } from '@/app/data/ingredients';
 
 export default function Home() {
+  const sortedIngredients = [...ingredients].sort((a, b) =>
+    a.name.localeCompare(b.name)
+  );
+
   return (
     <div className="container mx-auto px-4 py-8">
       <HeroSection />
-      <IngredientGrid ingredients={ingredients} />
+      <IngredientGrid ingredients={sortedIngredients} />
     </div>
   );
 }
