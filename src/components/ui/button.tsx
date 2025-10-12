8b430bfc5e5684b19a6aab551b051ce11a7d416d
Nero <ask.nero.now@gmail.com>
2025-10-12 23:30:10 +0000

    Try fixing this error: `Console Error: Hydration failed because the serv

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -41,18 +41,9 @@ export interface ButtonProps
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
-    if (asChild) {
-      return (
-        <Slot
-          className={cn(buttonVariants({ variant, size, className }))}
-          ref={ref}
-          {...props}
-        />
-      )
-    }
-
+    const Comp = asChild ? Slot : "button"
     return (
-      <button
+      <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
